0b9830a3d1abf558fb041a2c56115bf6fdb16c73
2Fl5S6jQy2 <serita@star-career.co.jp>
2022-12-19 10:38:09 +0900

    objectでstateを管理する

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import { useState } from "react";
-const App = (props) => {
-  const [name, setName] = useState(props.name);
-  const [price, setPrice] = useState(props.price);
 
-  const reset = () => {
-    setName(props.name);
-    setPrice(props.price);
-  };
+const App = (props) => {
+  const [state, setState] = useState(props);
+  const reset = () => setState(props);
+  const { name, price } = state;
   return (
     <>
       <p>
         現在の{name}は{price}円です
       </p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={() => setState({ ...state, price: price + 1 })}>
+        +1
+      </button>
+      <button onClick={() => setState({ ...state, price: price - 1 })}>
+        -1
+      </button>
       <button onClick={reset}>Reset</button>
       <input
         type="text"
         value={name}
-        onChange={(e) => setName(e.target.value)}
+        onChange={(e) => setState({ ...state, name: e.target.value })}
       />
     </>
   );
